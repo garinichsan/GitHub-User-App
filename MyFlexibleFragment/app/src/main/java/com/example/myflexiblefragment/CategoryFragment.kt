10c1c662442f7cbb29cae797917683e075c96931
Garin <garin.kra@gmail.com>
2020-12-29 20:51:22 +0700

    feat: add detail category fragment

--- MyFlexibleFragment/app/src/main/java/com/example/myflexiblefragment/CategoryFragment.kt
+++ MyFlexibleFragment/app/src/main/java/com/example/myflexiblefragment/CategoryFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.FragmentManager
 
 /**
  * A simple [Fragment] subclass.
@@ -28,7 +29,21 @@ class CategoryFragment : Fragment(), View.OnClickListener {
 
     override fun onClick(v: View?) {
         if (v?.id == R.id.btn_detail_category){
+            val mDetailCategoryFragment = DetailCategoryFragment()
 
+            val mBundle = Bundle()
+            mBundle.putString(DetailCategoryFragment.EXTRA_NAME, "Lifestyle")
+            val description = "Kategori ini akan berisi produk-produk lifestyle"
+
+            mDetailCategoryFragment.arguments = mBundle
+            mDetailCategoryFragment.description = description
+
+            val mFragmentManager = fragmentManager
+            mFragmentManager?.beginTransaction()?.apply {
+                replace(R.id.frame_container, mDetailCategoryFragment, DetailCategoryFragment::class.java.simpleName)
+                addToBackStack(null)
+                commit()
+            }
         }
     }
 
