1a442a0eaa3cbce4f24b513a1bbf5e23419bd73a
Garin <garin.kra@gmail.com>
2021-01-05 22:07:09 +0700

    feat: add search menu in action bar

--- GithubUser/app/src/main/java/gin/garin/githubuser/HomeActivity.kt
+++ GithubUser/app/src/main/java/gin/garin/githubuser/HomeActivity.kt
@@ -1,9 +1,14 @@
 package gin.garin.githubuser
 
 import android.annotation.SuppressLint
+import android.app.SearchManager
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import gin.garin.githubuser.databinding.ActivityHomeBinding
 
@@ -63,6 +68,34 @@ class HomeActivity : AppCompatActivity() {
             }
         })
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.option_menu, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.search).actionView as SearchView
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.queryHint = resources.getString(R.string.search_hint)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            /*
+            Gunakan method ini ketika search selesai atau OK
+             */
+            override fun onQueryTextSubmit(query: String): Boolean {
+                Toast.makeText(this@HomeActivity, query, Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            /*
+            Gunakan method ini untuk merespon tiap perubahan huruf pada searchView
+             */
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
+        return true
     }
 
 }
