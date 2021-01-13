5342c87f9b825b52d5293ece7597a7bba737d5db
Garin <garin.kra@gmail.com>
2021-01-13 17:51:55 +0700

    feat: add deeplink to access application from notification

--- GithubUser/app/src/main/java/gin/garin/githubuser/settings/SettingsActivity.kt
+++ GithubUser/app/src/main/java/gin/garin/githubuser/settings/SettingsActivity.kt
@@ -1,23 +1,37 @@
 package gin.garin.githubuser.settings
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
 import android.view.View
+import android.widget.Switch
+import androidx.core.app.NotificationCompat
 import androidx.core.view.get
+import gin.garin.githubuser.R
 import gin.garin.githubuser.databinding.ActivitySettingsBinding
 
 class SettingsActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivitySettingsBinding
 
+    companion object {
+        private const val NOTIFICATION_ID = 1
+        private const val CHANNEL_ID = "channel_01"
+        private const val CHANNEL_NAME = "githubuser channel"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.notificationSwitch.isChecked = false
 
         binding.cardLanguageSetting.setOnClickListener(this)
 
@@ -27,6 +41,45 @@ class SettingsActivity : AppCompatActivity(), View.OnClickListener {
         if (v == binding.cardLanguageSetting) {
             val mIntent = Intent(Settings.ACTION_LOCALE_SETTINGS)
             startActivity(mIntent)
+        }
+    }
+
+    fun setNotification(view: View) {
+
+        val status = binding.notificationSwitch.isChecked
+
+        if(status) {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.garin.gin/githubuser"))
+            val pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+            val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            val mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentIntent(pendingIntent)
+                .setSmallIcon(R.drawable.ic_notifications)
+                .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_notifications))
+                .setContentTitle(resources.getString(R.string.app_name))
+                .setContentText(resources.getString(R.string.reminder_text))
+                .setSubText(resources.getString(R.string.reminder))
+                .setAutoCancel(true)
+
+            /*
+            Untuk android Oreo ke atas perlu menambahkan notification channel
+            */
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                /* Create or update. */
+                val channel = NotificationChannel(
+                    CHANNEL_ID,
+                    CHANNEL_NAME,
+                    NotificationManager.IMPORTANCE_DEFAULT
+                )
+                channel.description = CHANNEL_NAME
+                mBuilder.setChannelId(CHANNEL_ID)
+                mNotificationManager.createNotificationChannel(channel)
+            }
+
+            val notification = mBuilder.build()
+
+            mNotificationManager.notify(NOTIFICATION_ID, notification)
         }
     }
 }
